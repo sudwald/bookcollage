8377a0102186ed0a737fae680f9dcfe63e2d3244
Sammy <s.challinor3@gmail.com>
2021-07-03 22:36:13 +0100

    Updated search so can search on title and author

--- src/App.js
+++ src/App.js
@@ -72,9 +72,10 @@ class App extends React.Component {
   }
   
   // fetches search results from the Google Books API
-  search(searchTerm) {
+  search(titleSearchTerm, authorSearchTerm) {
+    const combinedSearchTerm = titleSearchTerm + " " + authorSearchTerm
     // carries out the search using the indicated search term
-    fetch(`https://www.googleapis.com/books/v1/volumes?q=${searchTerm}&printType=books&maxResults=40`)
+    fetch(`https://www.googleapis.com/books/v1/volumes?q=${combinedSearchTerm}&intitle:${titleSearchTerm}&inauthor:${authorSearchTerm}&printType=books&maxResults=40`)
         .then(response => response.json())
         // takes only the .items[...] array from the json response and stores it in state.jsonArray, this is the only part of the json response we will work with
         .then(data => this.setState({jsonArray: data.items}))
--- src/SearchBar.js
+++ src/SearchBar.js
@@ -9,21 +9,27 @@ export class SearchBar extends React.Component {
         super(props)
         // searchTerm is initialised as empty
         this.state = {
-          searchTerm: ''
+          titleSearchTerm: '',
+          authorSearchTerm: ''
         }
-        this.handleChange = this.handleChange.bind(this)
+        this.handleTitleChange = this.handleTitleChange.bind(this)
+        this.handleAuthorChange = this.handleAuthorChange.bind(this)
         this.handleSearch = this.handleSearch.bind(this)
         this.handleKeyUp = this.handleKeyUp.bind(this)
       }
 
     // updates searchTerm as the user types so it is ready when they press search
-    handleChange(event) {
-        this.setState({searchTerm: event.target.value})
+    handleTitleChange(event) {
+        this.setState({titleSearchTerm: event.target.value})
       }
+
+    handleAuthorChange(event) {
+      this.setState({authorSearchTerm: event.target.value})
+    }
 
     //   calls the search function using the searchTerm
     handleSearch(event) {
-        this.props.onSearch(this.state.searchTerm)
+        this.props.onSearch(this.state.titleSearchTerm, this.state.authorSearchTerm)
     }
 
     handleKeyUp(event) {
@@ -40,7 +46,8 @@ export class SearchBar extends React.Component {
                     <h2>READ? TBR? ALL-TIME FAVES? MAKE AN EASY COLLAGE OF BOOK COVERS</h2>
                 </div>
                 {/* space for user to input their search term, calls handleChange as they type */}
-                <input className='searchText' placeholder='Search for a book' onChange={this.handleChange}  onKeyUp={this.handleKeyUp}/>
+                <input id='titleSearch' className='searchText' placeholder='Title' onChange={this.handleTitleChange}  onKeyUp={this.handleKeyUp}/>
+                <input id='authorSearch' className='searchText' placeholder='Author' onChange={this.handleAuthorChange}  onKeyUp={this.handleKeyUp}/>
                 {/* search button which invokes a search when clicked */}
                 <button onClick={this.handleSearch}>SEARCH</button>
             </div>
